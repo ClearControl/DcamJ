203edd9f90994686b873df8dfae685e8c22128fd
Loic Alain Royer (Scope Computer 1) <royerloic@gmail.com>
2013-04-08 22:28:29 +0200

    switch off debug

--- src/dcamj/DcamAcquisition.java
+++ src/dcamj/DcamAcquisition.java
@@ -34,7 +34,7 @@ public class DcamAcquisition implements Closeable
 	private TriggerType mTriggerType = TriggerType.Internal;
 
 	private boolean mShowErrors = false;
-	private boolean mDebug = true;
+	private boolean mDebug = false;
 
 	private DcamDevice mDcamDevice;
 	private DcamBufferControl mBufferControl;
